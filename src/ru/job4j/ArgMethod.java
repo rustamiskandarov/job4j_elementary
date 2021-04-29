e5f2c458ecbe88ad528c94cc7a410154179e1505
Rustam <rustpo4ta@mail.ru>
2021-04-29 17:02:10 +0300

    4.1. Аргументы

---
+++ src/ru/job4j/ArgMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name+", "+age+" age");
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+        ArgMethod.hello(name);
+        ArgMethod.hello(name, age);
+    }
+}
