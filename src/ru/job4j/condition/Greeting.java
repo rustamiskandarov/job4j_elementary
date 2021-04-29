905258caa49a3b9854ac2a372f36f3a924efd3a7
Rust <admin@helper.com>
2021-04-29 23:41:19 +0300

    9.1. String

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea =  "I like Java!";
+        idea = idea+"But I am a newbie";
+        int currentYear = 2021;
+        System.out.println(idea + " " + currentYear);
+    }
+}
