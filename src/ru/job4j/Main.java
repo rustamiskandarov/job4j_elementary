9b30ef94884cade3aef01587481f9895dc517bf4
Rustam <rustpo4ta@mail.ru>
2021-04-30 15:19:51 +0300

    0. Что такое тестирование

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,6 +2,14 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int expected2 = 3;
+        int result = Converter.rubleToDollar(180);
+        boolean passed2 = expected2 == result;
+        System.out.println("180 rubles are 3 dollars. Test result : " + passed2);
     }
 }
